8df2bd1f3470b9068932bb97c2ea329f1a35ddaa
Kalash <kalashjain513@gmail.com>
2023-01-29 22:43:36 +0530

    configurations for the chaser and controller

--- setup.py
+++ setup.py
@@ -20,6 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'spawner = turtlechaser.spawner:main',
+            'chaser = turtlechaser.controller:main'
         ],
     },
 )
